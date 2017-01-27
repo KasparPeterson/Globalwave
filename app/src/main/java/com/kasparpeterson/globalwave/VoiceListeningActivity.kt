df78c1bfa1b9eb66a76d4d43d6a5d2ded7d028e4
KasparPeterson <kasparpeterson@hotmail.com>
2017-01-27 21:10:04 +0200

    Adds kotlin with new launcher activity

---
+++ app/src/main/java/com/kasparpeterson/globalwave/VoiceListeningActivity.kt
@@ -0,0 +1,16 @@
+package com.kasparpeterson.globalwave
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by kaspar on 27/01/2017.
+ */
+class VoiceListeningActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_voice_listening)
+    }
+
+}
